ae7764f472869791defb14b97e9a2df7aafc57be
pranay <pranay@kiprosh.com>
2019-07-18 13:07:25 +0530

    set volume funcitonality added

--- src/components/VideoParent/VideoPlayer/index.tsx
+++ src/components/VideoParent/VideoPlayer/index.tsx
@@ -8,15 +8,24 @@ interface Props {
   setDuration: (videoDuration: any) => void;
 }
 
-class VideoPlayer extends React.Component<Props, any> {
+interface State {
+  volume: number;
+}
+
+class VideoPlayer extends React.Component<Props, State> {
   video: any;
   slider: any;
+  volumeSlider: any;
   constructor(props: Props) {
     super(props);
-    // create a ref to store the textInput DOM element
+    // create a ref to store the  DOM elements
     this.video = React.createRef();
     this.slider = React.createRef();
+    this.volumeSlider = React.createRef();
   }
+  state: State = {
+    volume: 0.5
+  };
   componentDidMount() {
     if (!this.video.current) return;
     else {
@@ -30,17 +39,17 @@ class VideoPlayer extends React.Component<Props, any> {
   componentDidUpdate() {
     if (!this.video.current) return;
     if (this.props.currentVideo.status) {
-      // this.props.setDuration(this.video.current.duration);
       this.video.current.play();
     } else {
       this.video.current.pause();
     }
   }
   setVideoDuration = () => {
+    //duration of video is set when browser is ready to play the video,onCanPlay event
     this.props.setDuration(this.video.current.duration);
   };
   setCurrentTime = (e: any) => {
-    // console.log(this.slider.current);
+    // change currentTime of video when slider changes or onTimeUpdate event of video
     if (e.target.type === 'range') {
       this.props.changeCurrentTime(this.slider.current.value);
       this.video.current.currentTime = this.slider.current.value;
@@ -49,9 +58,19 @@ class VideoPlayer extends React.Component<Props, any> {
     }
   };
 
-  setCurrentTimeFromSlider = () => {
-    console.log(this.slider.current.value);
-    this.props.changeCurrentTime(this.slider.current.value);
+  setVolume = (e: any) => {
+    if (e.target.type === 'range') {
+      this.setState(
+        { volume: parseFloat(this.volumeSlider.current.value) },
+        () => {
+          this.video.current.volume = parseFloat(
+            this.volumeSlider.current.value
+          );
+        }
+      );
+    } else {
+      this.video.current.volume = this.state.volume;
+    }
   };
 
   render() {
@@ -62,10 +81,12 @@ class VideoPlayer extends React.Component<Props, any> {
             <video
               width="500px"
               height="200px"
-              autoPlay
               src={this.props.currentVideo.source}
               ref={this.video}
-              onCanPlay={this.setVideoDuration}
+              onCanPlay={e => {
+                this.setVideoDuration();
+                this.setVolume(e);
+              }}
               onTimeUpdate={e => this.setCurrentTime(e)}
             />
             {this.props.currentVideo.status ? (
@@ -78,9 +99,19 @@ class VideoPlayer extends React.Component<Props, any> {
                     min="0"
                     max={this.props.currentVideo.duration.toString()}
                     value={this.props.currentVideo.currentTime.toString()}
-                    id="myRange"
                     step="1"
                     onChange={e => this.setCurrentTime(e)}
+                  />
+                </div>
+                <div className="volumeSlider">
+                  <input
+                    type="range"
+                    ref={this.volumeSlider}
+                    min="0.0"
+                    max="1.0"
+                    value={this.state.volume}
+                    step="0.1"
+                    onChange={this.setVolume}
                   />
                 </div>
               </>
@@ -98,6 +129,18 @@ class VideoPlayer extends React.Component<Props, any> {
                     step="1"
                     onChange={e => this.setCurrentTime(e)}
                   />
+
+                  <div className="volumeSlider">
+                    <input
+                      type="range"
+                      ref={this.volumeSlider}
+                      min="0.0"
+                      max="1.0"
+                      value={this.state.volume}
+                      step="0.1"
+                      onChange={this.setVolume}
+                    />
+                  </div>
                 </div>
               </>
             )}
